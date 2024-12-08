74371a488dcc96dd43ae29b3fcfa002f6b3610ce
whg <wanghg@skylinematrix.com>
2024-12-08 08:05:19 +0800

    Handle invalid commands

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,26 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+
+    public static void main(String[] args) {
         // Uncomment this block to pass the first stage
-        System.out.print("$ ");
+        print("$ ");
 
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
+        println("%s: node found", input);
     }
+
+    private static void print(String msg){
+        System.out.print(msg);
+    }
+
+    private static void println(String format, Object... args){
+        println(String.format(format, args));
+    }
+
+    private static void println(String msg){
+        System.out.println(msg);
+    }
+
 }
